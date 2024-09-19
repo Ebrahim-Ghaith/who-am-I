deb1394b96de9bc234a9d0d10ad7556161c91dcc
Ebrahim-Ghaith <84124169+Ebrahim-Ghaith@users.noreply.github.com>
2024-09-20 01:55:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is my Personality Quiz app!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
